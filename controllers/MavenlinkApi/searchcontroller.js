9f4d5b0caa4fa5bf56264d86cd0b9124b9ce2c4a
solrobson <solomon.robson@velir.com>
2016-05-10 16:52:15 -0400

    Implemented a search controller around the Mavenlink Search API.

---
+++ controllers/MavenlinkApi/searchcontroller.js
@@ -0,0 +1,40 @@
+﻿
+module.exports = function() {
+    'use strict';
+    var search = {};
+    var mlUrl = "https://api.mavenlink.com/api/v1/workspaces.json?search=";
+    var _callback;
+    var _accessToken = "";
+    
+    var callCallback = function (error, response, body) {
+        if (typeof(_callback) === "function") {
+            _callback(error, response, body);
+        }    
+    }
+
+    var singleSearch = function(workspaceName) {
+        var searchUrl = mlUrl + encodeURI(workspaceName);
+        var request = require('request');
+        request.get(searchUrl, function (error, response, body) {
+            callCallback(error, response, body);
+        }).auth(null, null, true, _accessToken);
+    };
+    
+
+    var mutliSearch = function(workspaceNames) {
+        
+    }
+
+    search.search = function (accessToken, workspaceName, callback) {
+        _callback = callback;
+        _accessToken = accessToken;
+
+        if (Array.isArray(workspaceName)) {
+            mutliSearch();
+        } else if (typeof(workspaceName) === "string") {
+            singleSearch(workspaceName);
+        }
+    }
+
+    return search;
+}();
--- routes/bulkEdit.js
+++ routes/bulkEdit.js
@@ -1,55 +1,66 @@
-﻿'use strict';
+﻿module.exports = function() {
+    'use strict';
 
-var express = require('express');
-var router = express.Router();
-var multer = require('multer');
-var clientCode = "8c2bed3570eab61d3271a9e5c02cd26960c5bd07b46455c0179106ba92d96bbc";
-var callbackUrl = encodeURIComponent("http://mavenlink.sol.velir.com/bulk.htm");
-var secret = "46ce07310cc2ac54c9563e391fd622cdb3599e9620a3ca3e9cc760e32b567e2b";
-var upload = multer({dest: 'uploads/'});
+    var express = require('express');
+    var router = express.Router();
+    var multer = require('multer');
+    var clientCode = "8c2bed3570eab61d3271a9e5c02cd26960c5bd07b46455c0179106ba92d96bbc";
+    var callbackUrl = encodeURIComponent("http://mavenlink.sol.velir.com/bulk.htm");
+    var secret = "46ce07310cc2ac54c9563e391fd622cdb3599e9620a3ca3e9cc760e32b567e2b";
+    var upload = multer({ dest: 'uploads/' });
 
-var Converter = require("csvtojson");
-var converter = new Converter.Converter({});
-var responseObj = {
-    successful: false,
-    content: 'error'
-};
-router.post('/csv', upload.single('cvsfile'), function (req, res, next) {
-    converter.fromFile(req.file.path, function (err, result) {
-        console.log(result);
-        responseObj.successful = true;
-        responseObj.content = result;
-        res.send(JSON.stringify(responseObj));
-   });
-    
-    
-});
-
-router.get('/auth/:code', function (req, res, next) {
-    var authUrl = "https://app.mavenlink.com/oauth/token?client_id=" + clientCode +
-             "&client_secret=" + secret +
-             "&grant_type=authorization_code" +
-             "&code=" + req.params.code +
-           "&redirect_uri=" + callbackUrl;
-     
-    var request = require('request');
-    request.post(authUrl, function (error, response, body) {
-        authResponse(error, response, body, res);
+    var Converter = require("csvtojson");
+    var converter = new Converter.Converter({});
+    var responseObj = {
+        successful: false,
+        content: 'error'
+    };
+    router.post('/csv', upload.single('cvsfile'), function(req, res, next) {
+        converter.fromFile(req.file.path, function(err, result) {
+            console.log(result);
+            responseObj.successful = true;
+            responseObj.content = result;
+            res.send(JSON.stringify(responseObj));
+        });
     });
-});
 
-var authResponse = function(error, response, body, res) {
-    if (error) {
-        res.send(JSON.stringify(responseObj));
-    } else if (response.statusCode === 200) {
-        responseObj.successful = true;
-        responseObj.content = JSON.parse(body);
-        res.send(JSON.stringify(responseObj));
-    } else {
-        responseObj.content = response.statusCode;
-        res.send(JSON.stringify(responseObj));
-    }
-};
+    router.get('/auth/:code', function(req, res, next) {
+        var authUrl = "https://app.mavenlink.com/oauth/token?client_id=" + clientCode +
+            "&client_secret=" + secret +
+            "&grant_type=authorization_code" +
+            "&code=" + req.params.code +
+            "&redirect_uri=" + callbackUrl;
 
+        var request = require('request');
+        request.post(authUrl, function(error, response, body) {
+            authResponse(error, response, body, res);
+        });
+    });
 
-module.exports = router;
+    router.get('/search/:accessKey/:term', function (req, res, next) {
+        var search = require('../controllers/MavenlinkApi/searchcontroller.js');
+        console.log(req.params.accessKey + "  search " + req.params.term);
+
+        var resultCb = function(error, response, body) {
+            res.send(JSON.stringify(body));
+        }
+        search.search(req.params.accessKey, req.params.term, resultCb);
+        
+        
+    });
+
+    var authResponse = function(error, response, body, res) {
+        if (error) {
+            res.send(JSON.stringify(responseObj));
+        } else if (response.statusCode === 200) {
+            responseObj.successful = true;
+            responseObj.content = JSON.parse(body);
+            res.send(JSON.stringify(responseObj));
+        } else {
+            responseObj.content = response.statusCode;
+            res.send(JSON.stringify(responseObj));
+        }
+    };
+
+    return router;
+}();
